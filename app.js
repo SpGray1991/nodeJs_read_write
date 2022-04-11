f82909fcab0cb1b7ce81daebabaaa9035b0d1545
Gray.1991 <Sergey.sp.1991@gmail.com>
2022-04-11 21:24:28 +0300

    dree

--- app.js
+++ app.js
@@ -30,3 +30,45 @@ storeData(obj, "./store/f.js"); */
 /* const obj = JSON.parse(fs.readFileSync("./store/f.js", "utf8"));
 
 console.log(obj); */
+
+//Рекурсивный поиск по каталогу
+
+/* const { resolve } = require("path");
+const { readdir } = require("fs").promises; 
+async function getFiles(dir) {
+  // читаем содержимое директории
+  const dirents = await readdir(dir, { withFileTypes: true });
+
+  // как и в прошлом примере проходимся по папкам
+  // и, при необходимости рекурсивно вызываем функцию
+  const files = await Promise.all(
+    dirents.map((dirent) => {
+      const res = resolve(dir, dirent.name);
+      return dirent.isDirectory() ? getFiles(res) : res;
+    })
+  );
+
+  // преобразуем массив файлов в одномерный
+  return Array.prototype.concat(...files);
+}
+
+
+getFiles("../../../test/")
+  .then((files) => console.log(files))
+  .catch((err) => console.error(err)); */
+
+// Лучший способ dree  с глубиной поиска и норм отображением
+
+/* const dree = require("dree");
+const tree = dree.scan("../../../test/");
+
+const options = {
+  followLinks: true,
+  depth: 3,
+  exclude: /dir_to_exclude/,
+  extensions: ["txt", "jpg"],
+};
+
+const string = dree.parseTree(tree, options);
+
+console.log(string); */
